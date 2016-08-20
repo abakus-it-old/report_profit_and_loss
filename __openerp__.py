3a8b05474676243d5b974c6e41ca72d0130b26d7
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-20 11:01:03 +0200

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -1,19 +1,17 @@
 ﻿{
     'name': 'Profit and Loss Report',
-    'version': '1.1',
+    'version': '9.0.1.0',
     'category': 'Accounting',
     'description': 
-    """
-    This modules adds a profit and loss report in detail. 
-    
-    This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
+    """This modules adds a profit and loss report in detail. 
+
+This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
     """,
-    'depends': [
-        'account', ],
-        'data': [
+    'depends': ['account'],
+    'data': [
         'wizard/profit_and_loss_view.xml',
         'reports/profit_and_loss.xml',
-    ],
+        ],
     'installable': True,
     'author': "Bernard DELHEZ, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
